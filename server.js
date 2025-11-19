b2d6a6587be4c0481c4bb80634f8fcb5b6a9b9c2
CHIN FOOG SIN <Chin.foog.sin@student.mmu.edu.my>
2025-11-20 01:19:36 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); 
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Chat Server running on http://localhost:${PORT}`);
+});
